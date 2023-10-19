75ef793222df54cb04b4cb98df14b0bae0e8e66c
Sravan <Sravan90149@gmail.com>
2023-10-19 15:54:42 +0530

    demo1

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,16 @@
+import 'cypress-xpath'
+
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://dev-in.kiiaccelerators.com/am/ui/login/')
+
+    const iframe = document.getElementById('yourIframeId'); // Replace with your iframe's ID
+iframe.contentWindow.postMessage('Your message', 'https://dev-in.kiiaccelerators.com');
+
+     cy.xpath("//input[@name='username']").clear();
+     cy.xpath("//input[@name='username']").type("user")
+    // cy.wait(2000)
+    //  cy.get("#user_pass").clear();   
+    //   cy.get("#user_pass").type("user")
+  })
+})
